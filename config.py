177b251579438720d0b87ef5962045be64e12444
yundaji <97157839+yundaji@users.noreply.github.com>
2026-06-21 22:27:56 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Google CSV
+CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQZWcEfix15x2dsT0sqTuSKLxVQ8KWgBA-hH3QAB7ztitH6rrlmWtXJT8aOYqc0E1lhEm4g1lcXH5YG/pub?output=csv"
+
+# 文章来源
+START_URL = "https://www.mhwmm.com/miandianxinwen/81512.html"
